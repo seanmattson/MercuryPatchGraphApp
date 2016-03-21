6f4dd46754caf7fb28a790605f74945fe35bdd15
Sean Mattson <sdmattson48@gmail.com>
2016-03-21 10:23:22 -0400

    Initial Commit

---
+++ Mercury Patch Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mercury Patch Prototype
+//
+//  Created by Sean Mattson on 3/21/16.
+//  Copyright © 2016 Sean Mattson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
